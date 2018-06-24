1d81e0d8e661bbb852f5d6442b6ea9d2489cf394
Trung Truc <1259035@itec.hcmus.edu.vn>
2018-06-25 01:12:32 +1200

    last submit app notes

--- app/src/main/java/com/example/trungtrucnguyen/appnotes/MainActivity.java
+++ app/src/main/java/com/example/trungtrucnguyen/appnotes/MainActivity.java
@@ -1,25 +1,61 @@
 package com.example.trungtrucnguyen.appnotes;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.support.v7.app.AlertDialog;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import android.content.Intent;
 
+import static android.widget.AdapterView.*;
+
 public class MainActivity extends AppCompatActivity {
-    ArrayList<String> notes = new ArrayList<String>();
+    static ArrayList<String> notes = new ArrayList<String>();
+    static ArrayAdapter arrayAdapter;
+    private int currentClickedNote;
+    static boolean addNewNote = false;
 
-    public void toSecondActivity(View view, String noteString) {
+    // Create ADD NOTE menu - Options Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
 
-        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-        intent.putExtra("noteString", noteString);
-        MainActivity.this.startActivity(intent);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.addNewNote){
+            currentClickedNote = -1;
+            addNewNote = true;
+            toSecondActivity();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void toSecondActivity() {
+
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        intent.putExtra("noteId", currentClickedNote);
+        startActivity(intent);
     }
 
     @Override
@@ -29,22 +65,59 @@ public class MainActivity extends AppCompatActivity {
 
         ListView noteList = (ListView) findViewById(R.id.noteList);
 
-        notes.add("Note A");
-        notes.add("Note B");
-        notes.add("Note C");
-        notes.add("Note D");
+//        notes.add("Note A");
 
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.trungtrucnguyen.appnotes", Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("notes", null);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
+        if (set != null) {
+            notes = new ArrayList(set);
+        }
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
 
         noteList.setAdapter(arrayAdapter);
 
         noteList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String noteString = notes.get(position);
-                toSecondActivity(view, noteString);
+                currentClickedNote = position;
+                addNewNote = false;
+                toSecondActivity();
             }
         });
+
+        noteList.setOnItemLongClickListener(new OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int pos, long id) {
+
+                final int itemDeleted = pos;
+
+                new AlertDialog.Builder(MainActivity.this).setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure?")
+                .setMessage("Do you want to delete this note?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Delete note
+                        notes.remove(itemDeleted);
+                        arrayAdapter.notifyDataSetChanged();
+
+                        // Adding sharedPreferences to store data
+                        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.trungtrucnguyen.appnotes", Context.MODE_PRIVATE);
+                        HashSet<String> set = new HashSet<>(MainActivity.notes);
+                        sharedPreferences.edit().putStringSet("notes", set).apply();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+
+                return true;
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/trungtrucnguyen/appnotes/SecondActivity.java
+++ app/src/main/java/com/example/trungtrucnguyen/appnotes/SecondActivity.java
@@ -1,16 +1,45 @@
 package com.example.trungtrucnguyen.appnotes;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.HashSet;
 
 
 public class SecondActivity extends AppCompatActivity {
+    EditText editText;
+    int noteId;
+
+    public void submitNote(View view) {
+        // Add new note
+        if (MainActivity.notes.size() == 0 || MainActivity.addNewNote){
+            MainActivity.notes.add(0, editText.getText().toString());
+        } else {
+            // Update current note
+            MainActivity.notes.set(noteId, String.valueOf(editText.getText()));
+        }
+        // Adding sharedPreferences to store data
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.trungtrucnguyen.appnotes", Context.MODE_PRIVATE);
+        HashSet<String> set = new HashSet<>(MainActivity.notes);
+        sharedPreferences.edit().putStringSet("notes", set).apply();
+//        Log.i("Set", set.toString());
+
+        MainActivity.arrayAdapter.notifyDataSetChanged();
+        // Back to Main Activity
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +50,18 @@ public class SecondActivity extends AppCompatActivity {
 
         // Declare intent to get value from main Activity
         Intent intent = getIntent();
-        Toast.makeText(SecondActivity.this, intent.getStringExtra("noteString"), Toast.LENGTH_LONG).show();
-        // End intent
+        // Declare EditText
+        editText = (EditText) findViewById(R.id.editText);
+        noteId = intent.getIntExtra("noteId", -1);
+
+        if (noteId != -1) {
+            editText.setText(MainActivity.notes.get(noteId));
+        } else {
+            editText.setText("");
+            noteId = MainActivity.notes.size();
+            Log.i("noteID: ", String.valueOf(noteId));
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+        }
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
